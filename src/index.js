6cd76d2f8c7429db6217d84f15b9d1bc58536441
makhitr <ring_ru@yahoo.com>
2021-02-14 16:09:01 +0300

    test done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length == 0) return 0;
+  let minNum = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (minNum > array[i]) minNum = array[i]
+  }
+  return minNum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length == 0) return 0;
+  let maxNum = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (maxNum < array[i]) maxNum = array[i];
+  }
+  return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length == 0) return 0;
+  let sumNum = 0;
+  let j = 0;
+  for (let i = 0; i < array.length; i++) {
+    sumNum += array[i];
+    j += 1;
+  }
+  return sumNum / j;
 }
